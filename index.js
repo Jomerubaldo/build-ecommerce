747bf2db0c33443e35abf873c4094457f7f32a3f
Jomer Ubaldo <Jomerubaldo@gmail.com>
2025-06-23 14:29:53 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("#btn1");
+
+const button1 = () => {
+  alert("THIS PAGE IS UNDER WORKING");
+};
+
+button.addEventListener("click", button1);
+
+// simple add button then if the user click the button inside the button text will appear
